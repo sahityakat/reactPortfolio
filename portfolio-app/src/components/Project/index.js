eca6cffbfa716b2bfa593434f72b5648782fc85f
Sahitya.katanguru <sahitya.katanguru@sunrun.com>
2022-07-10 19:18:17 -0700

    updating project links

--- portfolio-app/src/components/Project/index.js
+++ portfolio-app/src/components/Project/index.js
@@ -6,46 +6,83 @@ function Project() {
     <section>
       <div className="flex-row">
         <div>
-        <h4>Note Taker</h4>
-            <img
-              src={photo}
-              alt="Note Taker"
-              className="img-thumbnail mx-1"
-              onClick={() => window.open("https://github.com/sahityakat/noteTaker")}
-            />
+          <a
+            href="https://github.com/sahityakat/noteTaker"
+            id="footer"
+            target="_blank" rel="noreferrer"
+          >
+            <h4>Note Taker</h4>
+          </a>
+
+          <img
+            src={photo}
+            alt="Note Taker"
+            className="img-thumbnail mx-1"
+            onClick={() =>
+              window.open("https://github.com/sahityakat/noteTaker")
+            }
+          />
         </div>
         <div>
-        <h4>Weather Dashboard</h4>
-            <img
-              src={photo}
-              alt="Weather Dashboard"
-              className="img-thumbnail mx-1"
-              onClick={() => window.open("https://github.com/sahityakat/weatherDashboard")}
-            />
+          <a
+            href="https://github.com/sahityakat/weatherDashboard"
+            id="footer"
+            target="_blank" rel="noreferrer"
+          >
+            <h4>Weather Dashboard</h4>
+          </a>
+
+          <img
+            src={photo}
+            alt="Weather Dashboard"
+            className="img-thumbnail mx-1"
+            onClick={() =>
+              window.open("https://github.com/sahityakat/weatherDashboard")
+            }
+          />
         </div>
       </div>
       <div className="flex-row">
         <div>
-        <h4>ReadMe Generator</h4>
-            <img
-              src={photo}
-              alt="ReadMe Generator"
-              className="img-thumbnail mx-1"
-              onClick={() => window.open("https://github.com/sahityakat/readMeGenerator")}
-            />
+          <a
+            href="https://github.com/sahityakat/readMeGenerator"
+            id="footer"
+            target="_blank" rel="noreferrer"
+          >
+            {" "}
+            <h4>ReadMe Generator</h4>
+          </a>
+
+          <img
+            src={photo}
+            alt="ReadMe Generator"
+            className="img-thumbnail mx-1"
+            onClick={() =>
+              window.open("https://github.com/sahityakat/readMeGenerator")
+            }
+          />
         </div>
         <div>
-        <h4>Workday Scheduler</h4>
-            <img
-              src={photo}
-              alt="Workday Scheduler"
-              className="img-thumbnail mx-1"
-              onClick={() => window.open("https://github.com/sahityakat/workdayScheduler")}
-            />
+          <a
+            href="https://github.com/sahityakat/workdayScheduler"
+            id="footer"
+            target="_blank" rel="noreferrer"
+          >
+            {" "}
+            <h4>Workday Scheduler</h4>
+          </a>
+
+          <img
+            src={photo}
+            alt="Workday Scheduler"
+            className="img-thumbnail mx-1"
+            onClick={() =>
+              window.open("https://github.com/sahityakat/workdayScheduler")
+            }
+          />
         </div>
       </div>
     </section>
-    
   );
 }
 export default Project;
